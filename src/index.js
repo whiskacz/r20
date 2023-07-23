98f52dd5413f97c6d5eae61ddb361f34b8962533
whiskacz <pawelblasiak1985@gmail.com>
2023-07-23 18:27:27 +0200

    React Practice r20

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Moon from './svg/moon-svgrepo-com.svg';
+import Earth from './svg/planet-earth-svgrepo-com.svg';
+import Ship from './svg/space-ship-rocket-svgrepo-com.svg';
+import Penguin from './svg/penguin-svgrepo-com.svg'
+
+
+function App () {
+    
+    const [travel,setTravel] = useState(true)
+    const [status,setStatus] = useState("")
+
+    console.log(status)
+    
+    return (
+      <div className="wrapper">
+        <img src={Moon} alt="Moon" className='moon' />
+        <img src={Earth} alt="Earth" className='earth' />
+        {travel ? <img src={Ship} alt="Ship" className={`ship ${status}`} /> :
+        <img src={Penguin} alt="Penguin" className={`penguin ${status}`} />}
+        <div className='toMoon' 
+            onClick={() => {setStatus("flyMoon");
+                            setTimeout(() => {
+                            setTravel(!travel)    
+                            }, 3000);
+            
+            }}>To moon</div>
+        <div className='toEarth'onClick={() => {setStatus("flyEarth")
+    
+        }}>To earth</div>
+      </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />)
